7da92153e2aca6d4e079fa65b553c54e25473f94
leshau90 <ilman1990@gmail.com>
2016-06-22 02:02:52 +0700

    Keyboard Done..noEngine

--- app/src/main/java/com/tap/ilman/ta04/AppUtils.java
+++ app/src/main/java/com/tap/ilman/ta04/AppUtils.java
@@ -162,10 +162,10 @@ public class AppUtils {
         }
     }
 
-    public static float pixelsToSp(Context context, float px) {
-        float scaledDensity = context.getResources().getDisplayMetrics().scaledDensity;
-        return px/scaledDensity;
-    }
+//    public static float pixelsToSp(Context context, float px) {
+//        float scaledDensity = context.getResources().getDisplayMetrics().scaledDensity;
+//        return px/scaledDensity;
+//    }
 }
 
 interface DownloadAndRead {
